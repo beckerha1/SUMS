240100a134abde4eb9c851161729800bdb48df62
beckerha1 <harrison.x.becker@gmail.com>
2026-03-27 07:21:23 -0400

    Refactor puzzle data structure: replaced individual puzzle exports with a unified array of puzzle objects, each containing a name and grid. This change simplifies puzzle management and enhances scalability for future additions.

--- src/puzzles/new-full-puzzles.js
+++ src/puzzles/new-full-puzzles.js
@@ -1,167 +1,137 @@
-export const puzzle22 = [
-  [null, 9, null, null, null, "X", "X"],
-  [null, 3, null, null, null, null, "X"],
-  [1, 2, null, null, null, "X", "X"],
-  [21, 13, null, null, null, null, "X"],
+export const newFullPuzzles = [
+  { name: "20260327", grid: [
+  ["X", "X", "X", "X", null, "X", "X"],
+  ["X", "X", null, null, null, null, "X"],
+  ["X", "X", null, null, null, 22, null],
   [null, null, null, null, null, null, "X"],
-  ["X", null, 30, "X", "X", "X", "X"],
-  ["X", "X", "X", "X", "X", "X", "X"]
-];
-
-export const puzzle23 = [
-  ["X", null, null, null, null, null, null],
-  ["X", null, null, null, null, null, null],
-  ["X", "X", null, 10, null, 3, 2],
-  ["X", 36, 15, null, null, null, 1],
-  ["X", null, null, null, "X", null, null],
-  ["X", "X", "X", null, null, null, 24],
-  ["X", "X", "X", null, null, null, null]
-];
-
-export const puzzle24 = [
-  ["X", "X", "X", "X", "X", "X", "X"],
-  ["X", "X", "X", "X", "X", null, null],
-  ["X", "X", null, "X", null, null, null],
-  ["X", "X", "X", null, null, null, null],
-  ["X", null, null, "X", null, null, null],
-  ["X", 28, null, null, 1, null, null],
-  [null, null, null, 2, 3, null, null]
-];
-
-export const puzzle25 = [
-  [null, null, null, null, null, "X", "X"],
-  [null, 1, 3, null, null, null, "X"],
-  [null, null, 2, null, null, null, "X"],
-  [null, null, null, null, null, null, "X"],
-  [null, 16, 11, null, null, null, "X"],
-  [null, null, 22, null, 26, 38, "X"],
-  [null, 33, 30, "X", "X", "X", "X"]
-];
-
-export const puzzle26 = [
-  ["X", "X", "X", 26, null, "X", "X"],
-  ["X", 21, 11, 15, "X", "X", "X"],
-  ["X", null, null, null, null, null, "X"],
-  ["X", null, null, 2, null, "X", "X"],
-  ["X", null, null, 3, 1, "X", "X"],
-  ["X", null, null, 18, null, "X", "X"],
-  ["X", null, "X", null, null, null, "X"]
-];
-
-export const puzzle27 = [
-  ["X", "X", 33, null, null, 14, null],
-  ["X", "X", null, null, null, null, null],
-  ["X", "X", "X", 22, null, null, null],
-  ["X", "X", null, null, 9, null, 2],
-  ["X", "X", null, null, null, 3, 1],
-  ["X", "X", null, null, null, null, null],
-  ["X", "X", "X", null, null, null, null]
-];
-
-export const puzzle28 = [
-  ["X", 31, 13, null, null, null, null],
-  ["X", null, 24, null, "X", null, null],
-  ["X", null, null, null, 1, 2, null],
-  ["X", null, null, null, null, 3, null],
-  ["X", "X", null, null, 18, null, 9],
-  ["X", "X", "X", null, 21, "X", null],
-  ["X", "X", "X", "X", "X", "X", "X"]
-];
-
-export const puzzle29 = [
-  [null, null, null, null, null, null, "X"],
-  [null, 1, null, null, null, "X", "X"],
-  [null, 3, 2, null, null, 24, "X"],
-  ["X", null, null, null, 15, "X", "X"],
-  [null, null, null, 10, null, null, "X"],
-  [null, null, null, null, null, "X", "X"],
-  ["X", "X", "X", null, 35, null, "X"]
-];
-
-export const puzzle30 = [
-  ["X", "X", "X", "X", "X", "X", "X"],
-  ["X", "X", "X", "X", "X", 29, "X"],
-  ["X", "X", null, null, null, null, null],
-  ["X", "X", null, null, null, null, null],
-  ["X", null, null, null, null, null, null],
-  ["X", null, null, null, null, 3, 1],
-  ["X", null, null, null, null, 2, null]
-];
-
-export const puzzle31 = [
-  ["X", null, "X", null, null, null, 15],
-  ["X", 23, null, null, null, null, null],
-  [34, "X", null, null, null, 1, null],
-  ["X", "X", null, null, 3, 2, null],
-  ["X", "X", null, null, null, null, null],
-  ["X", "X", "X", 20, 27, null, 10],
-  ["X", "X", null, "X", "X", null, null]
-];
-
-export const puzzle32 = [
-  ["X", "X", "X", "X", "X", "X", "X"],
-  [null, null, null, null, "X", "X", "X"],
-  [null, null, 14, null, 22, "X", "X"],
-  [null, null, null, null, null, "X", "X"],
-  [1, 3, null, null, null, null, "X"],
-  [2, null, null, null, null, 32, "X"],
-  [null, null, 9, null, null, null, "X"]
-];
-
-export const puzzle33 = [
-  ["X", null, null, null, null, "X", "X"],
-  [null, null, null, null, "X", "X", null],
-  ["X", null, null, 1, null, null, "X"],
-  [null, "X", 2, 3, null, null, null],
-  [null, null, null, null, null, "X", null],
-  [null, null, null, null, 37, null, "X"],
-  [null, null, null, null, null, "X", "X"]
-];
-
-export const puzzle34 = [
-  ["X", "X", "X", null, 21, null, null],
-  ["X", "X", null, "X", null, null, null],
-  ["X", null, 12, null, null, 3, 1],
-  ["X", null, 27, null, null, 2, null],
-  ["X", "X", null, null, null, "X", null],
-  ["X", "X", "X", null, "X", 16, 19],
-  ["X", "X", "X", "X", "X", "X", "X"]
-];
-
-export const puzzle35 = [
-  [null, null, null, 2, 1, null, null],
-  [null, null, null, 3, null, "X", null],
-  [null, 17, null, 26, null, null, null],
+  [null, 14, "X", null, null, null, "X"],
+  [33, 9, 1, 3, null, null, null],
+  [null, "X", null, 2, null, null, "X"]
+] },
+  { name: "20260328", grid: [
+  [null, null, null, null, "X", 36, "X"],
+  [2, "X", null, null, null, 24, "X"],
+  [3, 1, null, null, 15, null, null],
   [null, null, null, null, null, null, null],
-  [null, null, null, 11, null, null, null],
-  ["X", 39, null, null, null, "X", "X"],
-  ["X", "X", "X", "X", null, "X", "X"]
+  [null, null, null, 10, null, null, "X"],
+  [null, "X", null, null, null, "X", "X"],
+  [null, "X", null, "X", "X", "X", "X"]
+] },
+  { name: "20260329", grid: [
+  [null, null, null, 17, null, "X", "X"],
+  [null, null, 2, null, null, null, null],
+  [null, 1, 3, null, 11, null, "X"],
+  [null, null, null, null, null, null, null],
+  [null, null, null, null, null, null, "X"],
+  [null, null, "X", null, null, null, "X"],
+  ["X", 26, "X", "X", 39, null, "X"]
+] },
+  { name: "20260330", grid: [
+  [null, "X", "X", "X", "X", "X", "X"],
+  [null, null, null, 11, null, "X", "X"],
+  ["X", null, null, null, null, "X", "X"],
+  [null, null, 1, 3, 26, null, "X"],
+  ["X", null, "X", 2, null, null, "X"],
+  ["X", "X", "X", 18, null, 15, "X"],
+  ["X", "X", "X", null, null, "X", 21]
+] },
+  { name: "20260331", grid: [
+  ["X", "X", null, "X", "X", null, null],
+  ["X", "X", "X", null, 18, "X", 9],
+  ["X", 24, null, null, null, null, "X"],
+  [null, 21, null, 1, 3, null, null],
+  ["X", 31, null, null, 2, "X", null],
+  ["X", null, null, null, "X", null, null],
+  ["X", "X", null, "X", "X", null, 13]
+] },
+  { name: "20260401", grid: [
+  ["X", "X", "X", "X", null, null, "X"],
+  ["X", "X", "X", null, "X", null, null],
+  ["X", "X", null, "X", null, null, null],
+  ["X", null, null, 3, 2, null, "X"],
+  ["X", null, null, 1, null, null, null],
+  ["X", null, null, "X", "X", null, "X"],
+  [28, null, null, null, null, "X", "X"]
+] },
+  { name: "20260402", grid: [
+  [32, "X", null, "X", null, null, "X"],
+  [null, null, null, 9, "X", null, "X"],
+  [null, null, 3, 2, 14, null, null],
+  [null, null, 1, null, null, null, "X"],
+  ["X", null, null, null, null, null, "X"],
+  [22, "X", null, null, null, "X", "X"],
+  ["X", "X", null, "X", "X", "X", "X"]
+] },
+  { name: "20260403", grid: [
+  [null, "X", null, null, null, null, 33],
+  [null, 14, "X", null, 9, null, 22],
+  [null, null, null, 1, 2, null, "X"],
+  ["X", "X", null, null, 3, null, null],
+  ["X", null, null, null, null, null, "X"],
+  ["X", "X", null, null, null, null, "X"],
+  ["X", "X", "X", null, "X", "X", "X"]
+] },
+  { name: "20260404", grid: [
+  [null, null, null, "X", null, null, null],
+  [null, null, "X", null, 22, 16, null],
+  [null, null, 1, 2, null, null, 26],
+  [null, null, null, 3, null, null, null],
+  ["X", null, 33, 11, null, "X", null],
+  ["X", null, 30, null, null, null, "X"],
+  ["X", "X", null, 38, "X", "X", "X"]
+] },
+  { name: "20260405", grid: [
+  ["X", null, null, null, "X", null, null],
+  [null, null, 2, null, null, null, "X"],
+  [null, 37, 3, 1, null, null, null],
+  [null, null, null, null, null, null, null],
+  [null, null, null, null, null, null, "X"],
+  ["X", null, null, null, "X", null, "X"],
+  ["X", "X", null, null, "X", "X", "X"]
+] },
+  { name: "20260406", grid: [
+  ["X", "X", null, "X", "X", "X", "X"],
+  ["X", null, null, 12, "X", "X", "X"],
+  [null, 27, null, null, "X", null, null],
+  [null, null, 3, 2, null, 16, "X"],
+  [21, "X", "X", 1, null, 19, "X"],
+  ["X", "X", null, null, null, null, "X"],
+  ["X", "X", null, null, "X", null, "X"]
+] },
+  { name: "20260407", grid: [
+  [null, null, null, null, 30, null, "X"],
+  [null, "X", null, null, 9, "X", null],
+  ["X", "X", null, 1, 2, null, "X"],
+  ["X", "X", null, null, 3, 13, null],
+  ["X", null, "X", null, "X", null, null],
+  ["X", "X", null, null, 21, "X", "X"],
+  ["X", null, null, null, "X", "X", "X"]
+] },
+  { name: "20260408", grid: [
+  [15, null, null, 27, "X", "X", "X"],
+  [null, null, null, "X", null, null, 23],
+  ["X", null, null, 1, null, null, null],
+  [null, 20, 3, 2, null, null, null],
+  [null, null, null, null, null, "X", "X"],
+  ["X", "X", 10, null, 34, "X", "X"],
+  ["X", null, null, null, "X", "X", "X"]
+] },
+  { name: "20260409", grid: [
+  ["X", "X", null, "X", null, "X", "X"],
+  ["X", "X", null, null, "X", null, "X"],
+  ["X", null, null, null, null, "X", null],
+  [null, null, "X", 2, null, null, "X"],
+  ["X", "X", 1, 3, null, null, null],
+  [null, null, null, null, 29, "X", null],
+  ["X", null, null, null, "X", "X", "X"]
+] },
+  { name: "20260410", grid: [
+  [null, null, "X", 10, null, "X", "X"],
+  [null, null, null, null, null, null, "X"],
+  [null, "X", null, 2, null, "X", null],
+  [null, null, null, 3, 1, null, null],
+  [35, 24, null, 15, "X", null, null],
+  ["X", "X", null, null, null, null, null],
+  ["X", "X", "X", null, "X", null, "X"]
+] }
 ];
-
-export const puzzle36 = [
-  ["X", 33, 14, null, null, 3, null],
-  ["X", null, "X", null, 2, 1, null],
-  ["X", null, null, 9, null, null, null],
-  ["X", null, null, null, null, null, null],
-  ["X", null, null, null, null, null, null],
-  ["X", "X", "X", 22, null, null, null],
-  ["X", "X", "X", "X", "X", "X", "X"]
-];
-
-
-// ENTRIES:
-//   { name: "20260304", grid: puzzle22, number: "22" },
-//   { name: "20260304", grid: puzzle23, number: "23" },
-//   { name: "20260304", grid: puzzle24, number: "24" },
-//   { name: "20260304", grid: puzzle25, number: "25" },
-//   { name: "20260304", grid: puzzle26, number: "26" },
-//   { name: "20260304", grid: puzzle27, number: "27" },
-//   { name: "20260304", grid: puzzle28, number: "28" },
-//   { name: "20260304", grid: puzzle29, number: "29" },
-//   { name: "20260304", grid: puzzle30, number: "30" },
-//   { name: "20260304", grid: puzzle31, number: "31" },
-//   { name: "20260304", grid: puzzle32, number: "32" },
-//   { name: "20260304", grid: puzzle33, number: "33" },
-//   { name: "20260304", grid: puzzle34, number: "34" },
-//   { name: "20260304", grid: puzzle35, number: "35" },
-//   { name: "20260304", grid: puzzle36, number: "36" }
