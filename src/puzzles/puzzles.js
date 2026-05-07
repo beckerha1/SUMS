f26d618bd1bb6c41c2eca0dd74439b872d19b366
beckerha1 <harrison.x.becker@gmail.com>
2026-05-07 00:03:46 -0400

    Update puzzles.js

--- src/puzzles/puzzles.js
+++ src/puzzles/puzzles.js
@@ -767,10 +767,10 @@ export const puzzles = [
   ] },
   { name: "20260507", grid: [
     ["X", "X", "X", null, "X", "X", "X"],
-    [null, null, null, 30, "X", "X", "X"],
-    [null, null, null, null, null, "X", null],
-    [null, null, null, null, null, "X", null],
-    [null, null, 2, null, null, "X", null],
+    [null, null, null, 30, "X", null, "X"],
+    [null, null, null, "X", null, "X", null],
+    [null, null, null, null, null, null, null],
+    [null, null, 2, null, null, null, null],
     [null, 1, 3, null, null, "X", "X"],
     [null, null, null, null, null, null, "X"]
   ] }
